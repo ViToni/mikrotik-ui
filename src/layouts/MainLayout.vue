36fc4353dfefefb4a5763ae3c782020b2d9f59a7
Victor Toni <victor.toni@gmail.com>
2023-02-27 19:57:52 +0100

    Move navigation drawer content into dedicated component

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -21,36 +21,7 @@
       show-if-above
       bordered
     >
-      <q-list>
-        <q-item>
-          <q-item-section>
-            <q-item-label>Navigation</q-item-label>
-          </q-item-section>
-        </q-item>
-
-        <q-item to="/logout">
-          <q-item-section avatar>
-            <q-icon name="logout" />
-          </q-item-section>
-          <q-item-section>Logout</q-item-section>
-        </q-item>
-      </q-list>
-
-      <q-expansion-item
-        v-if="0 < essentialLinks.length"
-        dense
-        expand-separator
-        icon="bookmark"
-        label="Essential Links"
-        default-closed
-        class="absolute-bottom"
-      >
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-expansion-item>
+      <NavigationDrawer />
     </q-drawer>
 
     <q-page-container>
@@ -66,15 +37,12 @@
 <script setup>
 import { ref } from "vue";
 
-import EssentialLink from "components/EssentialLink.vue";
+import NavigationDrawer from "./NavigationDrawer.vue";
 
 const drawerOpen = ref(false);
 function toggleDrawer() {
     drawerOpen.value = !drawerOpen.value;
 }
-
-const essentialLinks = [
-];
 </script>
 
 <style>
---
+++ src/layouts/NavigationDrawer.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-list>
+    <q-item>
+      <q-item-section>
+        <q-item-label>Navigation</q-item-label>
+      </q-item-section>
+    </q-item>
+
+    <q-item to="/logout">
+      <q-item-section avatar>
+        <q-icon name="logout" />
+      </q-item-section>
+      <q-item-section>Logout</q-item-section>
+    </q-item>
+  </q-list>
+
+  <q-expansion-item
+    v-if="0 < essentialLinks.length"
+    dense
+    expand-separator
+    icon="bookmark"
+    label="Essential Links"
+    default-closed
+    class="absolute-bottom"
+  >
+    <EssentialLink
+      v-for="link in essentialLinks"
+      :key="link.title"
+      v-bind="link"
+    />
+  </q-expansion-item>
+</template>
+
+<script setup>
+import EssentialLink from "components/EssentialLink.vue";
+
+const essentialLinks = [
+];
+</script>
